d17a16c8f2296288c62e353fac609ee9f9c803e9
rezoc <r.chikashua@gmail.com>
2022-12-25 20:29:39 +0400

    Command line rebuildCommand line rebuildCommand line rebuildCommand line rebuild

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ from keras.losses import MeanSquaredError
 from keras.metrics import RootMeanSquaredError, MeanAbsolutePercentageError
 from keras.optimizer_v2.adam import Adam
 from keras.saving.save import load_model
+from matplotlib import pyplot as plt
 
 import US
+
 absl.logging.set_verbosity(absl.logging.ERROR)
 modelPath = 'modelTbilisi1/'
 rebuild = False
@@ -39,41 +41,36 @@ df1['Troloff2'] = df1['Troloff'] ** 2
 # df1['Troloff3'] = df1['Troloff'] ** 3
 df.index = pd.to_datetime(df['Date'], format='%m/%d/%Y')
 df1.index = pd.to_datetime(df1['Date'], format='%m/%d/%Y')
+drivers = ['wf', 'mf', 'sf', 'IHT', 'wfy', 'dhdd1', 'hdd1t', 'Troloff', 'Troloff2', 'mx2', 'mn4', 'dcy', 'c7', 'cy',
+           'c']
+TestData = df.loc[:, drivers]
 
-TestData = df.loc[:,
-           ['wf', 'mf', 'sf', 'IHT', 'wfy', 'dhdd1', 'hdd1t', 'Troloff', 'Troloff2', 'mx2', 'mn4', 'dcy', 'c7', 'cy',
-            'c']]
-
-ForecastData = df1.loc[:, ['wf', 'mf', 'sf', 'IHT', 'wfy', 'dhdd1', 'hdd1t', 'Troloff', 'mx2', 'mn4']]
-
+ForecastData = pd.concat([TestData[len(TestData) - 2 * ws:], df1.loc[:,
+                                                             ['wf', 'mf', 'sf', 'IHT', 'wfy', 'dhdd1', 'hdd1t',
+                                                              'Troloff', 'Troloff2', 'mx2', 'mn4']]])
 mean = np.mean(TestData['c'])
 std = np.std(TestData['c'])
-print(mean, std)
 TestData['c'] = (TestData['c'] - mean) / std
 TestData['cy'] = (TestData['cy'] - mean) / std
 TestData['c7'] = (TestData['c7'] - mean) / std
-means = {'c': [mean, std]}
+
+means = {'c': [mean, std], 'cy': [mean, std], 'c7': [mean, std]}
+
 for col in TestData.columns:
     if col not in ['c', 'cy', 'c7']:
         TestData[col], m, s = US.normalizeArray(TestData[col])
         means[col] = [m, s]
 
-for col in ForecastData.columns:
-    ForecastData[col] = (ForecastData[col] - means[col][0]) / means[col][0]
-
+# with open(modelPath + "means.json", "w") as write_file:
+#     json.dump(means, write_file, indent=4)
 X1, y1 = US.dftoXy1(TestData, ws)
 
-print(X1.shape)
-print(y1.shape)
-
 dataL = len(y1)
-testL = 7
-X_train1, y_train1 = X1[0:dataL - 3], y1[0:dataL - 3]
-X_val1, y_val1 = X1[dataL - testL - 3:dataL - 3], y1[dataL - testL - 3:dataL - 3]
+testL = ws
+X_train1, y_train1 = X1[0:dataL], y1[0:dataL]
+X_val1, y_val1 = X1[dataL - testL:dataL], y1[dataL - testL:dataL]
 X_test1, y_test1 = X1[dataL - 3:], y1[dataL - 3:]
-print(X_train1.shape, y_train1.shape, X_val1.shape, y_val1.shape, X_test1.shape, y_test1.shape)
 neurs = len(X_train1[0][0])
-print(X_test1[0])
 
 if rebuild:
     modelDNN = Sequential()
@@ -111,13 +108,26 @@ if rebuild:
 
 modelDNN = load_model(modelPath)
 means = json.load(open(modelPath + "means.json", "r"))
-print(means)
-# xP = np.array([X_test1[2], X_test1[3]])
-# print(xP)
-# print(xP.shape)
-# yP = modelDNN.predict(xP) * means['c'][1] + means['c'][0]
-# print(yP)
-US.plot_predictions(modelDNN, X_test1, y_test1, 'Test Predictions', 'Actuals', 'model7',
-                    means['c'][0], means['c'][1],
-                    float(modelDNN.optimizer.lr), ep, ws, start=0, end=100)
+
+# US.plot_predictions(modelDNN, X_test1, y_test1, 'Test Predictions', 'Actuals', 'model7',
+#                     means['c'][0], means['c'][1],
+#                     float(modelDNN.optimizer.lr), ep, ws, start=0, end=100)
+
+for col in ForecastData.columns:
+    ForecastData[col] = (ForecastData[col] - means[col][0]) / means[col][1]
+
+for r in range(2 * ws, len(ForecastData)):
+    ForecastData['c7'][r] = ForecastData['c'][r - 7]
+    ForecastData['cy'][r] = ForecastData['c'][r - 1]
+    dcy = ForecastData['cy'][r] / ForecastData['cy'][r - 1]
+    ForecastData['dcy'][r] = - means['dcy'][0] / means['dcy'][1]
+    X1, y1 = US.dftoXy1(ForecastData[:r + 1], ws)
+    yP = modelDNN.predict(X1)
+    ForecastData['c'][r] = yP[len(yP) - 1]
+
+results = ForecastData['c'] * means['c'][1] + means['c'][0]
+print(results)
+plt.plot(results)
+plt.show()
+
 exit(0)
